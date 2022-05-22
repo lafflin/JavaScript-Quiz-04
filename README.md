77e09ad89cae0a0008452847750648f93b91372c
Max McLaughlin <maxwelljmack@gmail.com>
2022-05-22 16:54:00 -0700

    init, adding current progress on quiz

---
+++ README.md
@@ -0,0 +1,95 @@
+# Module 4 Challenge
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [User Story](#user-story)
+  - [Acceptance Criteria](#acceptance-criteria)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+  ​
+
+## Overview
+
+This weeks challenge was create from scratch a JavaScript quiz
+​
+
+### The challenge
+
+​Create a JavaScript quiz from scratch that would allow you to store your high-score alongside your initials
+
+### User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+### Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+​
+
+### Screenshot
+
+### Links
+
+​
+
+- Solution URL: [Solution]()
+- Live Site URL: [Site]()
+  ​
+
+## My process
+
+1.
+
+### Built with
+
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+- jQuery
+
+### What I learned
+
+-
+
+### Continued development
+
+-
+
+### Useful resources
+
+-
+
+## Author
+
+- Linkedin - [Max McLaughlin](https://www.linkedin.com/in/max-mcla/)
+
+## Acknowledgments
+
+-
---
+++ assets/script.js
@@ -0,0 +1,112 @@
+// start button content
+var mainEl = $("#container");
+var startButtonEl = $("<button>");
+var timerEl = $("#timer");
+var quizEl = $("#quiz");
+var resultsEl = $("$results");
+const questionsOptions = [
+	{
+		question: "What typecase do we use in JavaScript?",
+		options: ["Snake case", "Camel case", "All capitals", "All lower case"],
+		answer: "Camel case",
+	},
+	{
+		question: "What do you use to create an array?",
+		options: [
+			"( Parentheses )",
+			"{ Curly Braces }",
+			"[ Brackets ]",
+			"' Single Quotes '",
+		],
+		answer: "[ Brackets ]",
+	},
+	{
+		question: "What is the difference between '==' and '==='?",
+		options: [
+			"Double == compares the value, and triple === compares the value and type",
+			"Triple === compares the value only",
+			"Nothing, they are the same",
+			"Double == compares the types only",
+		],
+		answer:
+			"Double == compares the value, and triple === compares the value and type",
+	},
+	{
+		question: "What does 'this' refer to?",
+		options: [
+			"Refers to global variables",
+			"Refers to the object that 'this' this a property of",
+			"Refers to all local variables",
+			"Refers to what is directly above it",
+		],
+		answer: "Refers to the object that 'this' this a property of",
+	},
+	{
+		question: "Which of the following is not a way to get user input?",
+		options: ["Prompt", "Alert", "Confirm", "Ask"],
+		answer: "Ask",
+	},
+];
+let index = 0;
+startButtonEl.text("Start");
+startButtonEl.attr("id", "start");
+mainEl.append(startButtonEl);
+startButtonEl.on("click", function () {
+	startButtonEl.attr("id", "remove-display");
+	questions();
+	timerEl.append("Time Remaining: ");
+	timeInterval();
+});
+
+function timeInterval() {
+	let timer = 10;
+	let spanEl = $("<span></span>");
+	let interval = setInterval(function () {
+		if (timer === 0) {
+			index++;
+			questions();
+			clearInterval(interval);
+		}
+		spanEl.html(timer);
+		timerEl.append(spanEl);
+		timer--;
+	}, 1000);
+}
+
+function questions() {
+	mainEl.children().remove();
+	quizEl.children().remove();
+	let questionDiv = $("<div>  </div>");
+	questionDiv.html(questionsOptions[index].question);
+	mainEl.append(questionDiv);
+	let unorderedList = $("<ul> </ul>");
+	for (let i = 0; i < questionsOptions[index].options.length; i++) {
+		let listItem = $(`<li onclick = optionClicked(event)>  </li>`);
+		listItem.html(questionsOptions[index].options[i]);
+		unorderedList.append(listItem);
+		// only goes through the first two questions because the timer runs out, not sure how to make it go to the next question after 10s, then reset the timer for another 10s
+	}
+	mainEl.append(unorderedList);
+}
+
+score = 0;
+
+function optionClicked(event) {
+	console.log(event.target.innerHTML);
+	let feedback = $("<div> </div>");
+	if (event.target.innerHTML === questionsOptions[index].answer) {
+		feedback.attr("id", "correct");
+		feedback.html("That is the correct answer");
+		localStorage.setItem("Score: ", ++score);
+		// issue with the score adding multiple times still exists
+	} else {
+		feedback.attr("id", "incorrect");
+		feedback.html("That is the incorrect answer");
+	}
+	quizEl.children().remove();
+	quizEl.append(feedback);
+}
+// write results function here (waiting cause i am not sure how to make it go to results yet with the timer not working right rn)
+function results() {}
+
+// after timer runs out on final question, got to results page with final score, allow user to add initials, then log it to the userStorage
